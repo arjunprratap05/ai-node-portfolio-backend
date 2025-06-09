83089d3461c6858176b871cbfa2557aecec697d4
arjunprratap05 <108358230+arjunprratap05@users.noreply.github.com>
2025-06-09 19:03:13 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const chatRoutes = require('./routes/chatRoute'); 
+//const chatRoutes = require('./routes/chatRoute'); 
 
 const app = express();
 
